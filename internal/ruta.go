db9eb66bffd3eb4b3814913f8434655865d9d893
Marcos <75510647+marcosrmartin@users.noreply.github.com>
2022-12-01 14:14:39 +0100

    Añado peaje, y elimino el coste  #18 Para que ruta sea un Objeto-valor y se calcule desde fuera.

--- internal/ruta.go
+++ internal/ruta.go
@@ -2,15 +2,19 @@ package internal
 import "errors"
 
 type ConsumoVehiculo float32
+type Peaje float32
 
 type Ruta struct{
-	precio float32
 	tramos [] Tramo
+	peaje Peaje
 }
 
-func NewRuta(tramos [] Tramo) (*Ruta, error){
+func NewRuta(tramos [] Tramo, peaje Peaje) (*Ruta, error){
 	if len(tramos) == 0 {
 		return nil, errors.New("Una ruta debe contener mínimo un tramo.")
 	}
-	return &Ruta{0, tramos}, nil
+	if peaje < 0 {
+		return nil, errors.New("Un peaje no puede ser negativo.")
+	}
+	return &Ruta{tramos, peaje}, nil
 }
