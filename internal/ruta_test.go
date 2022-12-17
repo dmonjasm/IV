13eba2f43a8dbc2e4fdfe098bef3ca978653781b
Daniel Monjas <dmonjasm@gmail.com>
2022-12-17 19:36:39 +0100

    Añadida ruta a test #33

--- internal/ruta_test.go
+++ internal/ruta_test.go
@@ -8,5 +8,9 @@ import (
 )
 
 var _ = Describe("Ruta", func() {
-
+	var m_30 = NewTramo(3, 70)
+	var a_4 = NewTramo(285, 120)
+	var a_44 = NewTramo(117, 100)
+	var gr_30 = NewTramo(13, 90)
+	var madrid_granada = NewRuta([m_30,a_4,a_44,gr_30], 0.0)
 })
