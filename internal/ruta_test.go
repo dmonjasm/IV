2c26536148409772d5d2c0ebaa0e449015a6ccbd
Daniel Monjas <dmonjasm@gmail.com>
2022-12-19 13:25:57 +0100

    Añadidas rutas para lógica de negocio #31

--- internal/ruta_test.go
+++ internal/ruta_test.go
@@ -9,14 +9,33 @@ import (
 
 var _ = Describe("Ruta", func() {
 	var m_30, a_4, a_44, gr_30  internal.Tramo
-	var madrid_granada internal.Ruta
+	var madrid_granada_1 internal.Ruta
+
+	var a_42, cm_42, a_4_corta internal.Tramo
+	var madrid_granada_2 internal.Ruta
+
+	var m_301, cm_4005, n_401, n_420, n_432 internal.Tramo
+	var madrid_granada_3 internal.Ruta
+
 
 	BeforeEach(func() {
 		m_30, _ = 	internal.NewTramo(7, 70)
 		a_4, _ = internal.NewTramo(360, 120)
 		a_44, _ = internal.NewTramo(117, 100)
 		gr_30, _ = internal.NewTramo(45, 90)
-		madrid_granada, _ = internal.NewRuta([]internal.Tramo{m_30, a_4, a_44, gr_30}, 0.0)
+		madrid_granada_1, _ = internal.NewRuta([]internal.Tramo{m_30, a_4, a_44, gr_30}, 42.83)
+		
+		a_42 = internal.NewTramo(107, 80)
+		cm_42 = internal.NewTramo(85, 90)
+		a_4_corta = internal.NewTramo(165, 120)
+		madrid_granada_2, _ = internal.NewRuta([]internal.Tramo{m_30, a_42, cm_42, a_44, gr_30}, 5.0)
+
+		m_301, _ = internal.NewTramo(57.8, 60)
+		cm_4005, _ = internal.NewTramo(56, 80)
+		n_401, _ = internal.NewTramo(103, 90)
+		n_420, _ = internal.NewTramo(140, 90)
+		n_432, _ = internal.NewTramo(130, 90)
+		madrid_granada_3, _ = internal.NewRuta([]internal.Tramo{m_301, cm_4005, n_401, n_420, n_432}, 0.0)
 	})
 
 	Describe("Calculating total distance", func() {
